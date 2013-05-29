a5fbb71cd7219348ce7ab950b0f2b12bdd6ee578
flagadajones <flagadajones@aliceadsl.fr>
2013-05-29 18:03:41 +0200

    Radio

---
+++ telecommande/src/fr/fladajonesjones/MediaControler/activity/SettingsActivity.java
@@ -0,0 +1,48 @@
+package fr.fladajonesjones.MediaControler.activity;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import fr.fladajonesjones.MediaControler.R;
+
+public class SettingsActivity extends PreferenceActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        }
+    
+    /**
+     * Populate the activity with the top-level headers.
+     */
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+    
+    /**
+     * This fragment shows the preferences for the first header.
+     */
+    public static class Prefs1Fragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Make sure default values are applied.  In a real app, you would
+            // want this in a shared function that is used to retrieve the
+            // SharedPreferences wherever they are needed.
+//            PreferenceManager.setDefaultValues(getActivity(),
+//                    R.xml.advanced_preferences, false);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.fragmented_preferences);
+        }
+    }
+
+    
+
+  
+}
